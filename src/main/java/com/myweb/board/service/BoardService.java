88e71b7103f1ccf48456dbac5d7dbb9b1b6ee4f6
yewon <qjqmti0113@gmail.com>
2024-07-10 18:37:50 +0900

    test8

--- src/main/java/com/myweb/board/service/BoardService.java
+++ src/main/java/com/myweb/board/service/BoardService.java
@@ -7,18 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface BoardService {
-	//등록기능
-	void regist(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 	//목록조회
 	void getList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
-	//글 상세내용
-	void getContent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
-	//글 수정화면
-	void modify(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
-	//글 수정
-	void update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
-	//글 삭제
-	void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
 
 
--- src/main/java/com/myweb/controller/BoardController.java
+++ src/main/java/com/myweb/controller/BoardController.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 
-@WebServlet("*.board") // .board 끝나는 모든 요청은 서블릿으로 연결
+@WebServlet("*.board")
 public class BoardController extends HttpServlet {
 
 	public BoardController() {
@@ -31,7 +31,6 @@ public class BoardController extends HttpServlet {
 	
 	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//요청 분기
 		request.setCharacterEncoding("utf-8");
 		
 		String uri = request.getRequestURI(); //ip, port번호 제외된 주소
@@ -41,84 +40,15 @@ public class BoardController extends HttpServlet {
 		System.out.println(command);
 		System.out.println(request.getRemoteAddr());
 		
-		//boardservice선언
 		BoardService service;
-		
 		
 		if(command.equals("/board/list.board")) { //목록화면
 			
-			//mvc2의 기본이동은 forward이다
-			//서비스 영역을 거쳐서 목록 data를 가지고 감
 			service = new BoardServiceImpl();
 			service.getList(request, response);
 			
-		} else if(command.equals("/board/write.board") ) { //글 작성화면
-			
-//			HttpSession session = request.getSession();
-//			String user_id = (String)session.getAttribute("user_id");
-//			
-//			if(user_id == null) {
-//				response.sendRedirect("/MyWeb/index.jsp");
-//				return;
-//			}
-			
-			request.getRequestDispatcher("board_write.jsp").forward(request, response);
-		
-		} else if(command.equals("/board/registForm.board") ) { //글 등록
-			
-
-			
-			
-			service = new BoardServiceImpl();
-			service.regist(request, response);
-			
-		} else if(command.equals("/board/getContent.board")) { //글 상세 내용
-			
-			service = new BoardServiceImpl();
-			service.getContent(request, response);
-			
-		} else if(command.equals("/board/modify.board")) { //수정 화면으로
-			
-
-			
-			service = new BoardServiceImpl();
-			service.modify(request, response);
-			
-			
-		} else if(command.equals("/board/updateForm.board")) { //글 수정
-			
-
-			
-			service = new BoardServiceImpl();
-			service.update(request, response);
-			
-		} else if(command.equals("/board/delete.board")) { //삭제 처리
-			
-
-			
-			service = new BoardServiceImpl();
-			service.delete(request, response);
-					
-			
-			
-		}
-		
-		
-		
-		
+		} 
 	
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 }
