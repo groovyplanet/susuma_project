43fd2ee03e5e2c91600226b15b4f8aa0406a391f
cognipe <gusdyddla@gmail.com>
2024-07-16 19:05:27 +0900

    24.07.16

---
+++ src/main/webapp/resources/js/kim.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const postLinks = document.querySelectorAll(".content-mix a");
+
+    postLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            // event.preventDefault();
+
+            const contentBox = this.closest('.content-box');
+            const postContent = contentBox.querySelector(".post-content");
+            if (postContent) {
+                postContent.classList.toggle("open");
+            }
+        });
+    });
+});
