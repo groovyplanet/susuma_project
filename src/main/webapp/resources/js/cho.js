e4306e4d591cf9ffbdf418aaa20e10991005065a
Yewon Cho <qjqmti0113@gmail.com>
2024-07-18 03:08:12 +0900

    cho update

--- src/main/webapp/resources/js/cho.js
+++ src/main/webapp/resources/js/cho.js
@@ -396,6 +396,135 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
+
+
+    // 달력
+    const init = {
+        monList: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        maxDayList: ['31', '28', '31', '30', '31', '30', '31', '31', '30', '31', '30', '31'],
+        dayList: ['일', '월', '화', '수', '목', '금', '토'],
+        today: new Date(),
+        monForChange: new Date().getMonth(),
+        activeDate: new Date(),
+        getFirstDay: (yy, mm) => new Date(yy, mm, 1),
+        getLastDay: (yy, mm) => new Date(yy, mm + 1, 0),
+        nextMonth: function () {
+            let d = new Date();
+            d.setDate(1);
+            d.setMonth(++this.monForChange);
+            this.activeDate = d;
+            return d;
+        },
+        prevMonth: function () {
+            let d = new Date();
+            d.setDate(1);
+            d.setMonth(--this.monForChange);
+            this.activeDate = d;
+            return d;
+        },
+        addZero: (num) => (num < 10) ? '0' + num : num,
+        activeDTag: null,
+        getIndex: function (node) {
+            let index = 0;
+            while (node = node.previousElementSibling) {
+                index++;
+            }
+            return index;
+        }
+    };
+
+    const $calBody = document.querySelector('.cal-body');
+    const $btnNext = document.querySelector('.btn-cal.next');
+    const $btnPrev = document.querySelector('.btn-cal.prev');
+
+    function loadDate(date, dayIn) {
+        document.querySelector('.cal-month-selected').textContent = document.querySelector('.cal-month').textContent;
+        document.querySelector('.cal-date-selected').textContent = date;
+        document.querySelector('.cal-day-selected').textContent = init.dayList[dayIn];
+    }
+
+    function loadYYMM(fullDate) {
+        let yy = fullDate.getFullYear();
+        let mm = fullDate.getMonth();
+        let firstDay = init.getFirstDay(yy, mm);
+        let lastDay = init.getLastDay(yy, mm);
+        let markToday;  // for marking today date
+
+        if (mm === init.today.getMonth() && yy === init.today.getFullYear()) {
+            markToday = init.today.getDate();
+        }
+
+        document.querySelector('.cal-month').textContent = init.monList[mm];
+        document.querySelector('.cal-year').textContent = yy;
+
+        let trtd = '';
+        let startCount;
+        let countDay = 0;
+        for (let i = 0; i < 6; i++) {
+            if (countDay >= init.maxDayList[mm]) break;
+            trtd += '<tr>';
+            for (let j = 0; j < 7; j++) {
+                if (i === 0 && !startCount && j === firstDay.getDay()) {
+                    startCount = 1;
+                }
+                if (!startCount) {
+                    trtd += '<td><span>'
+                } else {
+                    let fullDate = yy + '.' + init.addZero(mm + 1) + '.' + init.addZero(countDay + 1);
+                    trtd += '<td class="day';
+                    trtd += (markToday && markToday === countDay + 1) ? ' today" ' : '"';
+                    trtd += ` data-date="${countDay + 1}" data-fdate="${fullDate}"><span>`;
+                }
+                trtd += (startCount) ? ++countDay : '';
+                if (countDay === lastDay.getDate()) {
+                    startCount = 0;
+                }
+                trtd += '</span></td>';
+            }
+            trtd += '</tr>';
+        }
+        $calBody.innerHTML = trtd;
+    }
+
+    function createNewList(val) {
+        let id = new Date().getTime() + '';
+        let yy = init.activeDate.getFullYear();
+        let mm = init.activeDate.getMonth() + 1;
+        let dd = init.activeDate.getDate();
+        const $target = $calBody.querySelector(`.day[data-date="${dd}"]`);
+
+        let date = yy + '.' + init.addZero(mm) + '.' + init.addZero(dd);
+
+        let eventData = {};
+        eventData['date'] = date;
+        eventData['memo'] = val;
+        eventData['complete'] = false;
+        eventData['id'] = id;
+        init.event.push(eventData);
+        $todoList.appendChild(createLi(id, val, date));
+    }
+
+    loadYYMM(init.today);
+    loadDate(init.today.getDate(), init.today.getDay());
+
+    $btnNext.addEventListener('click', () => loadYYMM(init.nextMonth()));
+    $btnPrev.addEventListener('click', () => loadYYMM(init.prevMonth()));
+
+    $calBody.addEventListener('click', (e) => {
+        var clickTd = e.target.tagName == 'TD' ? e.target : e.target.parentElement; //SPAN인 경우 TD 저장
+        if (clickTd.classList.contains('day')) {
+            if (init.activeDTag) {
+                init.activeDTag.classList.remove('day-active');
+            }
+            let day = Number(clickTd.textContent);
+            loadDate(day, clickTd.cellIndex);
+            clickTd.classList.add('day-active');
+            init.activeDTag = clickTd;
+            init.activeDate.setDate(day);
+        }
+    });
+
+
 }); /* DOMContentLoaded */
 
 
