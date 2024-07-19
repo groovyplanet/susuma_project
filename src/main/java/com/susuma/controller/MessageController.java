273483c7ffc8e19c4d4de94c1681be003999dbce
yewon <qjqmti0113@gmail.com>
2024-07-20 04:51:38 +0900

    0720 cho update message.jsp

--- src/main/java/com/susuma/controller/MessageController.java
+++ src/main/java/com/susuma/controller/MessageController.java
@@ -1,9 +1,9 @@
 package com.susuma.controller;
 
+import com.susuma.message.model.MessageDTO;
 import com.susuma.message.service.MessageService;
 import com.susuma.message.service.MessageServiceImpl;
-import com.susuma.message.model.MessageDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,26 +11,25 @@ import jakarta.servlet.http.*;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/Susuma/master/messages")
+@WebServlet("/master/messages") // 'Susuma/' 삭제
 public class MessageController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private MessageService messageService = new MessageServiceImpl();
-    private ObjectMapper mapper = new ObjectMapper();
+	private static final long serialVersionUID = 1L;
+	private MessageService messageService = new MessageServiceImpl();
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<MessageDTO> messages = messageService.getMessages();
-        String json = mapper.writeValueAsString(messages);
-        response.setContentType("application/json");
-        response.getWriter().write(json);
-    }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		int userNo = Integer.parseInt(request.getParameter("userNo"));
+//        int getMeNo = Integer.parseInt(request.getParameter("getMeNo"));
+//        List<MessageDTO> messages = messageDAO.getMessages(userNo, getMeNo);
+		List<MessageDTO> messages = messageService.getMessages(request, response);
+		String json = new Gson().toJson(messages);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().print(json);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int masterNo = Integer.parseInt(request.getParameter("masterNo"));
-        int clientNo = Integer.parseInt(request.getParameter("clientNo"));
-        String messageText = request.getParameter("messageText");
-        messageService.addMessage(masterNo, clientNo, messageText);
-        response.setStatus(HttpServletResponse.SC_OK);
-    }
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		messageService.sendMessage(request, response);
+	}
 }
