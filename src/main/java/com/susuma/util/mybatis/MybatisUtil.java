b113c5dd3fdf6a9859df52060e26e59980d3daf9
yewon <qjqmti0113@gmail.com>
2024-07-21 22:55:19 +0900

    0721 cho update - web.xml, MybatisUtil.java

--- src/main/java/com/susuma/util/mybatis/MybatisUtil.java
+++ src/main/java/com/susuma/util/mybatis/MybatisUtil.java
@@ -1,23 +1,30 @@
 package com.susuma.util.mybatis;
 
 import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.io.Resources;
 
 public class MybatisUtil {
-    private static SqlSessionFactory sqlSessionFactory;
+	
+	private static SqlSessionFactory sqlSessionFactory;
 
-    static {
-        try {
-            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	static {
+		
+		try {
+			String resource = "mybatis/config/mybatis-config.xml";
+			
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
-    public static SqlSessionFactory getSqlSessionFactory() {
-        return sqlSessionFactory;
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static SqlSessionFactory getSqlSessionFactory() {
+		return sqlSessionFactory;
+	}
+	
 }
